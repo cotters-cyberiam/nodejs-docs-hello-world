46c6ace9035ff6672a4fa03c958b4ea0fecf133f
cotters-cyberiam <111287035+cotters-cyberiam@users.noreply.github.com>
2022-08-16 12:06:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!!! From Jenkins & Docker, DockerHub and of course Node.js");
+    response.end("Hello World!!!! From Jenkins & Docker, DockerHub and of course Node.js");
 });
 
 const port = process.env.PORT || 80;
